392ce40fcd4789be3be619c0332a5a86a33dff18
edward tremblay <edwardtremblay@MacBookPro.lan>
2025-11-25 11:33:22 -0800

    feat: Add Vercel Analytics for usage tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 
@@ -12,7 +13,12 @@ try {
   }
   
   const root = ReactDOM.createRoot(rootElement)
-  root.render(<App />)
+  root.render(
+    <>
+      <App />
+      <Analytics />
+    </>
+  )
   
   console.log('App rendered successfully')
 } catch (error) {
